f9218b29626b8f896bdb205de48df2c1531aa86c
YueChangxin <auauau@wust.edu.cn>
2025-02-28 19:41:24 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/auauau/RM_Auto_Aim/src/armor_detector
-# Build directory: /home/auauau/RM_Auto_Aim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(DetectorNumberClassifierTest "/home/auauau/RM_Auto_Aim/build/armor_detector_test")
-set_tests_properties(DetectorNumberClassifierTest PROPERTIES  _BACKTRACE_TRIPLES "/home/auauau/RM_Auto_Aim/src/armor_detector/CMakeLists.txt;79;add_test;/home/auauau/RM_Auto_Aim/src/armor_detector/CMakeLists.txt;0;")
